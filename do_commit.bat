c323d156293211c723cdc4027649d792ddd3c1ff
Cosmin Toma <vv.ep.team@gmail.com>
2026-05-15 11:46:12 +0300

    chore: remove temp do_commit.bat

--- do_commit.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "C:\VV_GITHUB\vv-lea"
-git add online/index.html
-git status
-git commit -m "LEA Premium UX: toast notifications, CP modal, AI badge, loading states"
-git push origin main
-echo DONE
